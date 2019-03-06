dc6d8a3cbde0f4aaa1218a3a0be05b271199a91e
Mihaly Nemeth <nemeth.mihaly@z3systems.hu>
2019-03-06 17:03:51 +0100

    Updated sample file

--- TFLight/samples/infinity_mirror_speaker.sample.h
+++ TFLight/samples/infinity_mirror_speaker.sample.h
@@ -12,7 +12,7 @@ void DefaultLights(TFLights *light) {
 	light->setRGB(1, 14, 12, 2,  "RGB#2");
 	
 	//NeoPixel (WS2812b) cimezheto ledszalag (ez fut a hangszorok korul)
-	//light->setPixel((uint8_t)index,  (uint8_t)pin, (int)pixelcount,  (char[30])name, (bool) shiftPixels) //10-pin 
+	//light->setPixel((uint8_t)index,  (uint8_t)pin, (int)pixelcount,  (char[30])name, (bool) shiftPixels) 
 	//shiftPixels=Lehetoseg van egy aldozati pixellel megoldani a 3.3V-5V logikai szintemelest
 	//ebben az esetben az elso pixelt nem hasznaljuk. Bovebb leiras a linken:
 	//https://hackaday.com/2017/01/20/cheating-at-5v-ws2812-control-to-use-a-3-3v-data-line/
@@ -25,11 +25,23 @@ void DefaultLights(TFLights *light) {
 	
 	//light->setPixelGroup((uint8_t)grp_index, (uint8_t)mainPixelIndex, (int)startPixel, (int)endPixel, (char[30])name);
 	//hangfal 1, a mely hangszoro koruli resz
-	light->setPixelGroup(0, 2, 0,  6,  "Bass#1");
+	light->setPixelGroup(0, 2, 0,  39,  "Bass#1");
 	//hangfal 1, a kozep/magas koruli resz
-	light->setPixelGroup(1, 3, 7, 13,  "Mid/High#1");
+	light->setPixelGroup(1, 3, 40, 71,  "Mid/High#1");
 	//hangfal 2, a mely hangszoro koruli resz
-	light->setPixelGroup(2, 2, 0,  6,  "Bass#2");
+	light->setPixelGroup(2, 2, 72,  111,  "Bass#2");
 	//hangfal 2, a kozep/magas koruli resz
-	light->setPixelGroup(3, 3, 7, 13,  "Mid/High#2");
+	light->setPixelGroup(3, 3, 112, 144,  "Mid/High#2");
+	
+	//ezzel a paranccsal beallitjuk hogy indulaskor
+	//hang alapu vezerlesre kapcsoljon minden eszkoz. 
+	//igy, ha aram ala helyezitek akkor egyszerre villogni fog, nem kell a TFcontrollerrel atallitani
+	//parameter sorrend: mode,type,param1,param2,param3,param4	bovebben a tflights.h es tflights.cpp fajlokban
+	//biztositjuk, hogy csak elso indulaskor fusson le, igy a TFControllerbol utolag at lehet majd allitani az alapertelmezett indulast
+	if (!Config.exists("SD0") || Config.getString("SD0") == "") {
+		Config.add("SD0", "4,1,0,0,0,0");
+		Config.add("SD1", "4,1,0,0,0,0");
+		Config.add("SD2", "4,1,0,0,0,2");
+		Config.save();
+	}
 }
